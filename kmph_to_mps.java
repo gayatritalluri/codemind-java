504ca798a5b254891b35d4edcd0510d476956d4f
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2024-01-08 22:08:56 +0530

    import java.util.Scanner; public class Convert{     public static void main (String [] args){         float k;         Scanner n  = new Scanner(System.in);         float m = n.nextFloat();         k=(5*m)/18;         System.out.printf("%.2f",k);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Convert{
+    public static void main (String [] args){
+        float k;
+        Scanner n  = new Scanner(System.in);
+        float m = n.nextFloat();
+        k=(5*m)/18;
+        System.out.printf("%.2f",k);
+    }
+}
