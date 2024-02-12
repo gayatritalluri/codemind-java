d60e73a1a72baaf86fcd7d99405f3af0eba8d292
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2024-02-12 22:01:40 +0530

    import java.util.Scanner; public class Subscription{ public static void main(String[] args){    Scanner sc = new Scanner(System.in);    int x,y;
        x=sc.nextInt();
        y=sc.nextInt();
       int k = Math.abs(x-y);
       System.out.println(k);
    
       
        
      }
    }

---
+++ Donation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Subscription{
+public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   int x,y;
+   
+    x=sc.nextInt();
+    y=sc.nextInt();
+   int k = Math.abs(x-y);
+   System.out.println(k);
+
+   
+    
+  }
+}
