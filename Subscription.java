7e00494d8703c3b54dc1c7027365c5e87647dcbe
gayatritalluri <119866807+gayatritalluri@users.noreply.github.com>
2024-02-12 21:54:07 +0530

    import java.util.Scanner; public class Subscription{ public static void main(String[] args){    Scanner sc = new Scanner(System.in);    int n;
        n=sc.nextInt();
      
        if(n>=40){
            System.out.println("YES");
        }
        else{
            System.out.println("NO");
        }
    
       
        
      }
    }

---
+++ Subscription.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Subscription{
+public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   int n;
+   
+    n=sc.nextInt();
+  
+    if(n>=40){
+        System.out.println("YES");
+    }
+    else{
+        System.out.println("NO");
+    }
+
+   
+    
+  }
+}
